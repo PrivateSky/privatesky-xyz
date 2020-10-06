64e53918bad54ac65d198c2992eaf98ddd882a4e
Deny Pătrașcu <denypatrascu@gmail.com>
2020-10-06 12:43:41 +0300

    cardinal update

--- cardinal/index-f603e042.js
+++
@@ -1,8 +0,0 @@
-import { A as ActiveRouter } from './active-router-f16f4518.js';
-import './match-path-760e1797.js';
-
-function injectHistory(Component) {
-    ActiveRouter.injectProps(Component, ['history', 'location']);
-}
-
-export { injectHistory as i };
