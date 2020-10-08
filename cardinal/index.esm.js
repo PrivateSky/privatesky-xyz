0ac619e6d3bf47b50fd1d150daebbfefd07036af
Deny Pătrașcu <denypatrascu@gmail.com>
2020-10-08 15:42:38 +0300

    psk-details update

--- cardinal/index.esm.js
+++ cardinal/index.esm.js
@@ -1,5 +1,1 @@
-import './index-14684944.js';
-import './active-router-f16f4518.js';
-import './index-f603e042.js';
-import './match-path-760e1797.js';
-import './location-utils-fea12957.js';
+import"./p-83ba3037.js";
