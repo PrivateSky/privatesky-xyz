384d0d934f339e2f6a655f7407c01d0561d25929
Deny Pătrașcu <denypatrascu@gmail.com>
2020-12-02 14:09:50 +0200

    psk-barcode-scanner documentation update

--- scripts/controllers/BarcodeController.js
+++ scripts/controllers/BarcodeController.js
@@ -1,18 +1,21 @@
 import ContainerController from "../../cardinal/controllers/base-controllers/ContainerController.js";
 
-export default class BarcodeController extends ContainerController{
+export default class BarcodeController extends ContainerController {
+	getModel() {
+		return {
+			isScannerActive: false,
+			scannedData: ''
+		}
+	}
+
 	constructor(element) {
 		super(element);
-		this.setModel({
-			scannerIsOn:false,
-			data:"1234567890",
-			scannedData:"",
-			gs1datamatrix:"(01)03594455500071(17)220506(21)0100110082245392(10)6006495"
-		})
+
+		this.setModel(this.getModel());
 
 		this.on("toggle-scanner", () => {
-			this.model.scannerIsOn = !this.model.scannerIsOn;
-		})
-
+			this.model.isScannerActive = !this.model.isScannerActive;
+			this.model.scannedData = '';
+		});
 	}
 }
